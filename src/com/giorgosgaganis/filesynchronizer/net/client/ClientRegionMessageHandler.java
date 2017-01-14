6676739f767149b5087a9555e1f1c927f3d720ee
Giorgos Gaganis <gaganis@yahoo.com>
2017-01-14 18:52:20 +0200

    WIP

---
+++ src/com/giorgosgaganis/filesynchronizer/net/client/ClientRegionMessageHandler.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 Giorgos Gaganis
+ *
+ * This file is part of odoxSync.
+ *
+ * odoxSync is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * odoxSync is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with odoxSync.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.giorgosgaganis.filesynchronizer.net.client;
+
+import com.giorgosgaganis.filesynchronizer.DirectorySynchronizer;
+
+import javax.ws.rs.client.Client;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by gaganis on 14/01/17.
+ */
+public class ClientRegionMessageHandler {
+    private static final Logger logger = Logger.getLogger(DirectorySynchronizer.class.getName());
+
+    private final Client restClient;
+
+    public ClientRegionMessageHandler(Client restClient) {
+        this.restClient = restClient;
+    }
+
+    public void submit(ClientRegionMessage clientRegionMessage) {
+        if (logger.isLoggable(Level.FINE)) {
+            logger.fine("ClientRegionMessager submited for processing ["
+                    + clientRegionMessage + "]");
+        }
+
+    }
+}
